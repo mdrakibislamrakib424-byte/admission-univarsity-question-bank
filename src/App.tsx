d2ba8526e24c7159752a47e09883ee3781d274b7
mdrakibislamrakib424-byte <mdrakibislamrakib424@gmail.com>
2026-09-02 22:18:59 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { BottomNav } from './components/BottomNav';
 import { BadgeToast } from './components/BadgeToast';
+import { ScoreSync } from './components/ScoreSync';
+import { PushInit } from './components/PushInit';
+import { DailyProgressAutoSync } from './components/DailyProgressAutoSync';
+import { AdMobInit } from './components/AdMobInit';
+import { InterstitialAdManager } from './components/InterstitialAdManager';
 import { Home } from './pages/Home';
 import { Browse } from './pages/Browse';
 import { Practice } from './pages/Practice';
@@ -18,10 +23,26 @@ import { Predictions } from './pages/Predictions';
 import { Leaderboard } from './pages/Leaderboard';
 import { Community } from './pages/Community';
 import { Badges } from './pages/Badges';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { BanglaSection } from './pages/BanglaSection';
+import { EnglishSection } from './pages/EnglishSection';
+import { AdminExam } from './pages/AdminExam';
+import { ExamList } from './pages/ExamList';
+import { ExamTake } from './pages/ExamTake';
+import { ExamResults } from './pages/ExamResults';
+import { DailyLesson } from './pages/DailyLesson';
+import { Subscription } from './pages/Subscription';
+import { AdminSubscriptions } from './pages/AdminSubscriptions';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-paper text-ink-900 dark:bg-ink-900 dark:text-ink-100">
+      <AdMobInit />
+      <InterstitialAdManager />
+      <ScoreSync />
+      <PushInit />
+      <DailyProgressAutoSync />
       <BadgeToast />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -40,9 +61,19 @@ export default function App() {
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/community" element={<Community />} />
         <Route path="/badges" element={<Badges />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/bangla" element={<BanglaSection />} />
+        <Route path="/english" element={<EnglishSection />} />
+        <Route path="/admin-exam" element={<AdminExam />} />
+        <Route path="/exam-list" element={<ExamList />} />
+        <Route path="/exam/:examId" element={<ExamTake />} />
+        <Route path="/exam-results" element={<ExamResults />} />
+        <Route path="/daily" element={<DailyLesson />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/admin-subscriptions" element={<AdminSubscriptions />} />
       </Routes>
       <BottomNav />
     </div>
   );
 }
-
