043ed7996ed7679895aa0922ee33d44466af20c4
mdrakibislamrakib424-byte <mdrakibislamrakib424@gmail.com>
2026-09-02 22:15:22 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AppDataProvider } from './context/AppDataContext';
+import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <AppDataProvider>
-        <App />
-      </AppDataProvider>
+      <AuthProvider>
+        <AppDataProvider>
+          <App />
+        </AppDataProvider>
+      </AuthProvider>
     </HashRouter>
   </React.StrictMode>
 );
